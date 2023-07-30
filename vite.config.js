88b607a842a705cc6dce3fffe186b2ba4d7201bb
Abishek P.Y <abishek@trainn.co>
2023-07-30 19:54:58 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/weather-app/'
-    : '/',
+  base: '/weather-app/'
 })
